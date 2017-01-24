4021dde8f260c64c85bb1481d3f50042ce9a8002
Phelipe de Sterlich <p.desterlich@gmail.com>
2017-01-24 18:30:51 +0100

    commit iniziale

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps")]
+    public class CampsController: Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok(new {Name = "Phelipe", FavouriteColor = "Blue"});
+        }
+    }
+}
