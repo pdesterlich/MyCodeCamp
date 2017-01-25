e2f19c972eb24dab338e021e335daed6898d29fc
Phelipe de Sterlich <p.desterlich@gmail.com>
2017-01-25 22:53:33 +0100

    lezione 4.6

--- MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -69,5 +69,33 @@ namespace MyCodeCamp.Controllers
 
             return BadRequest();
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Camp model)
+        {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+
+                if (oldCamp == null) return NotFound($"Could not find a camp with an ID of {id}");
+
+                oldCamp.Name = model.Name ?? oldCamp.Name;
+                oldCamp.Description = model.Description ?? oldCamp.Description;
+                oldCamp.Location = model.Location ?? oldCamp.Location;
+                oldCamp.Length = model.Length > 0 ? model.Length : oldCamp.Length;
+                oldCamp.EventDate = model.EventDate != DateTime.MinValue ? model.EventDate : oldCamp.EventDate;
+
+                if (await _repo.SaveAllAsync())
+                {
+                    return Ok(oldCamp);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return BadRequest("Couldn't update Camp");
+        }
     }
 }
